091f7c3f71590e83f7895d58ad1cfd5bee115c4d
amit <amitpatel_7@msn.com>
2020-02-27 08:02:30 -0800

    implemented progress radial dial for % completed

--- client/src/components/Projects/Projects.js
+++ client/src/components/Projects/Projects.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import './Projects.sass';
 import './media_queries.sass';
-
 import projectService from '../../services/projectService';
+
+import VisibilitySensor from 'react-visibility-sensor';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 const Projects = () => {
   // Hook for getting projects
@@ -33,7 +36,19 @@ const Projects = () => {
         <h3 className="name">{name}</h3>
         <h4 className="deadline">{deadline}</h4>
         <div className="progress">
-          <div className="progress_dial">{completed_percentage + '%'}</div>
+          <div className="progress_dial">
+            <VisibilitySensor>
+              {({ isVisible }) => {
+                const percentage = isVisible ? completed_percentage : 0;
+                return (
+                  <CircularProgressbar
+                    value={percentage}
+                    text={`${percentage}%`}
+                  />
+                );
+              }}
+            </VisibilitySensor>
+          </div>
           <div className="tasks_completed">
             <h4>Tasks Completed:</h4>
             <div className="ratio">{tasks_completed + '/' + total_tasks}</div>
