b0e747e9f8d16d8b00d919b4ef928d120d802295
amit <amitpatel_7@msn.com>
2020-02-27 18:21:42 -0800

    added modal to Add New Project button

--- client/src/components/Projects/Projects.js
+++ client/src/components/Projects/Projects.js
@@ -10,9 +10,17 @@ import 'react-circular-progressbar/dist/styles.css';
 import { easeQuadInOut } from "d3-ease";
 import AnimatedProgressProvider from "./AnimatedProgressProvider";
 
+import { Button, Modal } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const Projects = () => {
   // Hook for getting projects
   const [projects, setprojects] = useState(null);
+
+  // hooks for add project modal
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   useEffect(() => {
     if(!projects) {
@@ -82,8 +90,24 @@ const Projects = () => {
     <div className="Projects">
       <h1 className="pageTitle">Projects</h1>
       <div className="searchBar">
-        <button className="add_project">Add New Project</button>
+        <button className="add_project" onClick={handleShow}>Add New Project</button>
       </div>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
       {/* Display all projects */}
       <div className="list">
         {(projects && projects.length > 0) ? 
