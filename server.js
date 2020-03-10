33f20195ed5e69c6faaca2fc468c8a32dedb3d82
amit <amitpatel_7@msn.com>
2020-03-09 17:48:17 -0700

    added URI to connect when using Starbucks URI

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ app.use(express.json()); // Makes sure that it comes back as json
 
 // MongoDB Atlas URI to connect to cluster
 const connectURI = 'mongodb+srv://Amit:watertemple89@cluster0-d2htt.mongodb.net/Project_Management_App?retryWrites=true&w=majority';
+const connectURI_Starbucks = 'mongodb://Amit:watertemple89@cluster0-shard-00-00-d2htt.mongodb.net:27017,cluster0-shard-00-01-d2htt.mongodb.net:27017,cluster0-shard-00-02-d2htt.mongodb.net:27017/Project_Management_App?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
 
-mongoose.connect(connectURI, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(connectURI_Starbucks, {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(projectRouter);
 
