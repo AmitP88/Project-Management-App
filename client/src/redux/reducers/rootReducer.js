1558d48d168dc56c4fa7089d457069ef7a59fde8
amit <amitpatel_7@msn.com>
2020-03-11 09:07:36 -0700

    moved initialState to store.js and exported it from there

--- client/src/redux/reducers/rootReducer.js
+++ client/src/redux/reducers/rootReducer.js
@@ -1,14 +1,5 @@
+import { initialState } from '../store/store';
 import ADD_PROJECT_SUBMIT from '../actions/addProjectSubmit';
-// import { compose } from 'redux';
-
-export const initialState = {
-  addProjectForm: {
-    name: '',
-    deadline: '',
-    tasks_completed: 0,
-    total_tasks: 0
-  }
-}
 
 const rootReducer = (state = initialState, action) => {
   if(action.type === ADD_PROJECT_SUBMIT){
--- client/src/redux/store/store.js
+++ client/src/redux/store/store.js
@@ -1,6 +1,15 @@
 import { createStore } from 'redux';
 import rootReducer from '../reducers/rootReducer';
 
+export const initialState = {
+  addProjectForm: {
+    name: '',
+    deadline: '',
+    tasks_completed: 0,
+    total_tasks: 0
+  }
+}
+
 const store = createStore(rootReducer);
 
 export default store;
