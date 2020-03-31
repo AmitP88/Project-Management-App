e316640065ccdb1e794a858b542d10613533ba7f
amit <amitpatel_7@msn.com>
2020-03-31 04:00:12 -0700

    implemented Material UIs AppBar component for the header

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import { Switch, Route } from 'react-router-dom';
 import './App.sass';
 import './media_queries.sass';
 
+// import app header
+import AppBar from './components/AppBar/AppBar';
+
 // import page components
 import Home from './components/Home/Home';
 import Projects from './components/Projects/Projects';
@@ -11,6 +14,7 @@ import ProjectPage from './components/ProjectPage/ProjectPage';
 const App = () => {
   return (
     <div className="App">
+      <AppBar />
       <Switch>
         <Route path='/' component={Home} exact />
         <Route path='/projects' component={Projects} />
