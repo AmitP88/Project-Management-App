9a216e98c23953f09bb8b328fcc8d91e46e25cfa
amit <amitpatel_7@msn.com>
2020-03-31 11:29:44 -0700

    changed h1 text in Home component

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div className="Home">
-      <h1>WELCOME HOME!</h1>
-      <Link to='/projects'>
-        <button className="projects_page">Go to Projects</button>      
-      </Link>
+      <h1>(Dashboard goes in this page)</h1>
     </div>
   );
 }
