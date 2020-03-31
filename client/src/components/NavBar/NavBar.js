3549dc0d2b26908bfc396f14c52134a31ef41e93
amit <amitpatel_7@msn.com>
2020-03-31 11:26:26 -0700

    implemented NavLink components from React-Router into NavBar component

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -33,24 +34,27 @@ const NavBar = () => {
     <Box display={{ xs: 'block', sm: 'none' }}>
       <AppBar position="fixed" color="primary" className={classes.appBar} style={{ backgroundColor: '#311b92' }}>
         <Toolbar>
-          <Box display='flex' flexWrap='wrap' css={{ maxWidth: 70, margin: 10 }}>
-            <Box mx='auto' css={{ width: 45 }}>
-              <DashboardIcon className={classes.icon} />            
+          <NavLink to='/' style={{ textDecoration: 'none', color: 'white' }} activeStyle={{ color: '#84ffff' }} exact>
+            <Box display='flex' flexWrap='wrap' css={{ maxWidth: 70, margin: 10 }}>
+              <Box mx='auto' css={{ width: 45 }}>
+                <DashboardIcon className={classes.icon} />            
+              </Box>
+              <Typography className={classes.title} variant='caption' noWrap>
+                Dashboard
+              </Typography>          
             </Box>
-            <Typography className={classes.title} variant='caption' noWrap>
-              Dashboard
-            </Typography>          
-          </Box>
+          </NavLink>
 
-          <Box display='flex' flexWrap='wrap' css={{ maxWidth: 70, margin: 10 }}>
-            <Box mx='auto' css={{ width: 55 }}>
-              <LibraryBooksIcon className={classes.icon} />            
+          <NavLink to='/projects' style={{ textDecoration: 'none', color: 'white' }} activeStyle={{ color: '#84ffff' }} exact>
+            <Box display='flex' flexWrap='wrap' css={{ maxWidth: 70, margin: 10 }}>
+              <Box mx='auto' css={{ width: 55 }}>
+                <LibraryBooksIcon className={classes.icon} />            
+              </Box>
+              <Typography className={classes.title} variant='caption' noWrap>
+                Projects
+              </Typography>          
             </Box>
-            <Typography className={classes.title} variant='caption' noWrap>
-              Projects
-            </Typography>          
-          </Box>
-
+          </NavLink>
 
         </Toolbar>
       </AppBar>      
