d7cd57d49399c83dcd161da60bb2ab4a9d971c9c
amit <amitpatel_7@msn.com>
2020-04-14 20:52:25 -0700

    replaced CKEditor component with TextField component for project name portion of project page

--- client/src/components/ProjectPage/ProjectPage.js
+++ client/src/components/ProjectPage/ProjectPage.js
@@ -17,6 +17,7 @@ import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Card from '@material-ui/core/Card';
+import TextField from '@material-ui/core/TextField';
 
 import 'date-fns';
 import DateFnsUtils from '@date-io/date-fns';
@@ -40,6 +41,9 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.text.secondary,
     margin: 5,
   },
+  heading: {
+    fontSize: 'large'
+  }
 }));
 
 const ProjectPage = () => {
@@ -63,39 +67,10 @@ const ProjectPage = () => {
               <NavigateBeforeIcon />
               Back to Projects
             </Link>
-            {/**
-              <h1 className="project_name">{selected_project.name}</h1>
-            */}
-            <CKEditor
-              editor={InlineEditor}
-              data={`<h1>${selected_project.name}</h1>`}
-              config={
-                {
-                  toolbar: ['heading', '|', 'bold', '|' ,'italic', 'undo', 'redo'],
-                  heading: {
-                    options: [
-                      { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
-                      { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
-                      { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' }
-                    ]
-                  }
-                }
-              }
-              onInit={(editor) => {
-                // You can store the "editor" and use when it is needed.
-                console.log( 'Editor is ready to use!', editor );                
-              }}
-              onChange={(event, editor) => {
-                const data = editor.getData();
-                console.log({event, editor, data});
-              }}
-              onBlur={(event, editor) => {
-                console.log('Blur.', editor);
-              }}
-              onFocus={(event, editor) => {
-                console.log('Focus.', editor);
-              }}
-            />
+
+            <div className="heading">
+              <TextField id="project_name" defaultValue={selected_project.name} />            
+            </div>
 
 
 
