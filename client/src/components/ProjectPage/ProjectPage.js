1beb623415ea89db4d81c0365ff49fb53481afff
amit <amitpatel_7@msn.com>
2020-04-15 19:15:59 -0700

    commented out onInit, onChange, onBlur, and onFocus attributes for CKEditor component to clear up dev console

--- client/src/components/ProjectPage/ProjectPage.js
+++ client/src/components/ProjectPage/ProjectPage.js
@@ -119,20 +119,20 @@ const ProjectPage = () => {
               <CKEditor
                 editor={InlineEditor}
                 data={'<p>What is the project about?</p>'}
-                onInit={(editor) => {
-                  // You can store the "editor" and use when it is needed.
-                  console.log( 'Editor is ready to use!', editor );                
-                }}
-                onChange={(event, editor) => {
-                  const data = editor.getData();
-                  console.log({event, editor, data});
-                }}
-                onBlur={(event, editor) => {
-                  console.log('Blur.', editor);
-                }}
-                onFocus={(event, editor) => {
-                  console.log('Focus.', editor);
-                }}
+                // onInit={(editor) => {
+                //   // You can store the "editor" and use when it is needed.
+                //   console.log( 'Editor is ready to use!', editor );                
+                // }}
+                // onChange={(event, editor) => {
+                //   const data = editor.getData();
+                //   console.log({event, editor, data});
+                // }}
+                // onBlur={(event, editor) => {
+                //   console.log('Blur.', editor);
+                // }}
+                // onFocus={(event, editor) => {
+                //   console.log('Focus.', editor);
+                // }}
               />
             </Paper>
           </Card>
